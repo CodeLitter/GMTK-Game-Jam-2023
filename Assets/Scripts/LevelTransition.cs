5c28ebb4558cfdd3213590215465eeb8cad5873d
nrc.dev <ncontrerasdev@gmail.com>
2023-07-08 16:01:53 -0500

    Feat:  Fade in/out on transiton/level-load

--- Assets/Scripts/LevelTransition.cs
+++ Assets/Scripts/LevelTransition.cs
@@ -2,18 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelTransition : MonoBehaviour
 {
+    public Image fadeInOutImage;
     public string levelName = "PleaseChangeMe";
+
+    private bool loadingScene = false;
     private string menuSceneName = "MainMenu";
+
+    private void Awake()
+    {
+        fadeInOutImage.color = Color.black;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(fadeImage(fadeOut: false));
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (!loadingScene && collision.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene(levelName);
+            StartCoroutine(fadeImage(fadeOut: true));
+            StartCoroutine(loadSceneAsync());
+            loadingScene = true;
         }
+    }
+
+    public void loadMainMenuScene()
+    {
+        SceneManager.LoadScene(menuSceneName);
     }
 
     public void manualLevelTransition()
@@ -24,5 +45,38 @@ public class LevelTransition : MonoBehaviour
     public void quitGame()
     {
         Application.Quit();
+    }
+
+    IEnumerator fadeImage(bool fadeOut)
+    {
+        float fadeInOutTime = 1.0f;
+        float elapsedTime = 0.0f;
+        Color c = fadeInOutImage.color;
+
+        while (elapsedTime <= fadeInOutTime)
+        {
+            yield return null;
+            elapsedTime += Time.deltaTime;
+            if (fadeOut)
+              c.a = Mathf.Clamp01(elapsedTime / fadeInOutTime);
+            else
+              c.a = 1.0f - Mathf.Clamp01(elapsedTime / fadeInOutTime);
+            fadeInOutImage.color = c;
+        }
+    }
+
+    IEnumerator loadSceneAsync()
+    { 
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(levelName);
+        asyncLoad.allowSceneActivation = false;
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+            if (asyncLoad.progress >= 0.9f && fadeInOutImage.color.a >= 1.0f)
+                asyncLoad.allowSceneActivation = true;
+        }
+        loadingScene = false;
+
     }
 }
