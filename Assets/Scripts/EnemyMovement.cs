8c776956fcbb2274d2efd556bac87c6971b2b163
nrc.dev <ncontrerasdev@gmail.com>
2023-07-08 23:28:31 -0500

    Refactor:  Major refactor to enemy movement, allows chasing player and navigation of Y axis

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,37 +4,103 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class EnemyMovement : MonoBehaviour 
+public class EnemyMovement : MonoBehaviour
 {
-    public bool movingLeft;
-    public Rigidbody2D rigidbody2D;
-    public Transform patrolPointLeft;
-    public Transform patrolPointRight;
+   public bool patroling;
+   public bool movingLeft;
+   public bool movingDown;
+   public bool chaseTargetX;
+   public bool chaseTargetY;
+   public Rigidbody2D rigidbody2D;
+   public Transform patrolPointLeft;
+   public Transform patrolPointRight;
+   public Vector2 speed = Vector2.one;
 
-    public Vector2 speed = Vector2.one;
-    private Vector2 velocity;
+   private bool pursuingTarget = false;
+   private Transform target;
+   private Vector2 velocity;
 
-    private void Awake()
-    {
-        rigidbody2D = GetComponent<Rigidbody2D>();
-    }
+   private void Awake()
+   {
+      rigidbody2D = GetComponent<Rigidbody2D>();
+   }
 
-    // Update is called once per framed
-    private void FixedUpdate()
-    {
-        velocity.x = 30.0f * (movingLeft ? -1.0f : 1.0f);
-        var currentVelocity = rigidbody2D.velocity;
-        currentVelocity.x = velocity.x * (speed.x * Time.fixedDeltaTime);
-        rigidbody2D.velocity = currentVelocity / rigidbody2D.mass;
-        
-        if (movingLeft && (rigidbody2D.transform.position.x <= patrolPointLeft.position.x))
-        {
+   // Update is called once per framed
+   private void FixedUpdate()
+   {
+      if (!pursuingTarget)
+      {
+         if (movingLeft)
+            target = patrolPointLeft;
+         else
+            target = patrolPointRight;
+      }
+      moveTowardsTarget(target);
+   }
+
+   private void OnTriggerEnter2D(Collider2D collision)
+   {
+      if (collision.CompareTag("Player"))
+      {
+         target = collision.gameObject.transform;
+         pursuingTarget = true;
+      }
+   }
+
+   private void OnTriggerExit2D(Collider2D collision)
+   {
+      if (collision.CompareTag("Player") && patrolPointRight != null)
+      {
+         target = (Vector3.Distance(transform.position, patrolPointLeft.position) >= Vector3.Distance(transform.position, patrolPointRight.position) ? patrolPointLeft : patrolPointRight);
+         pursuingTarget = false;
+      }
+      else if (collision.CompareTag("Player"))
+      {
+         target = null;
+         pursuingTarget = false;
+      }
+   }
+
+   private void moveTowardsTarget(Transform target)
+   {
+      var currentVelocity = rigidbody2D.velocity;
+
+      if (chaseTargetX)
+      {
+         velocity.x = 30.0f * (movingLeft ? -1.0f : 1.0f);
+         currentVelocity.x = velocity.x * (speed.x * Time.fixedDeltaTime);
+      }
+
+      if (chaseTargetY)
+      {
+         velocity.y = 30.0f * (movingDown ? -1.0f : 1.0f);
+         currentVelocity.y = velocity.y * (speed.y * Time.fixedDeltaTime);
+      }
+
+      rigidbody2D.velocity = currentVelocity / rigidbody2D.mass;
+
+      if (chaseTargetX)
+      {
+         if (movingLeft && (rigidbody2D.transform.position.x <= target.position.x))
+         {
             movingLeft = false;
-        }
-        else if (!movingLeft && (rigidbody2D.transform.position.x >= patrolPointRight.position.x))
-        {
+         }
+         else if (!movingLeft && (rigidbody2D.transform.position.x >= target.position.x))
+         {
             movingLeft = true;
-        }
+         }
+      }
 
-    }
+      if (chaseTargetY)
+      {
+         if (movingDown && (rigidbody2D.transform.position.y <= target.position.y))
+         {
+            movingDown = false;
+         }
+         else if (!movingDown && (rigidbody2D.transform.position.y >= target.position.y))
+         {
+            movingDown = true;
+         }
+      }
+   }
 }
